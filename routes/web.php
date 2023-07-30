097dda055db4500681d5e0428196595498836d87
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-30 08:38:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', 'ProductsController@home')->name('web.home');
+// Route::get('home', 'ProductsController@home')->name('web.home');
