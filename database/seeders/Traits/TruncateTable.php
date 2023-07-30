52a55f7cd84dbbc9b90f757ccd8783832ff63a0b
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-30 16:57:48 +0800

    update

---
+++ database/seeders/Traits/TruncateTable.php
@@ -0,0 +1,13 @@
+<?
+
+namespace database\seeders\Traits;
+
+use Illuminate\Support\Facades\DB;
+
+trait TruncateTable
+{
+  protected function truncate($table)
+  {
+    DB::table($table)->truncate();
+  }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,9 +5,11 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use database\seeders\Traits\TruncateTable;
 
 class UserSeeder extends Seeder
 {
+    // use TruncateTable;
     /**
      * Run the database seeds.
      *
