b2304628d3c8d39a218c697b34479a6c42adab2f
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-08-01 08:14:32 +0800

    Modify post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,8 +13,22 @@ class Post extends Model
         'body' => 'array',
     ];
 
+    public function getTitleUpperCaseAttribute()
+    {
+        return strtoupper($this->title);
+        /*
+        *
+        * $ php artisan tinker
+        * Psy Shell v0.11.19 (PHP 7.4.33 — cli) by Justin Hileman
+        * > \App\Models\Post::find(1)->title_upper_case
+        * = "UNTITLED_02222"
+        *
+        */
+    }
+
     public function users()
     {
+
         return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
     }
 
