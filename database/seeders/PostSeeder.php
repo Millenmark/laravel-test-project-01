ebe729b29cd3a28350d00c7407fe8dd07e8c21fb
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-08-03 15:19:02 +0800

    Update PostSeeder.php

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,9 +19,12 @@ class PostSeeder extends Seeder
         //
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::table('posts')->truncate();
-        Post::factory(10)->state([
-            'title' => 'untitled_02222'
-        ])->create();
+        Post::factory(10)
+            // ->has(Comment::factory(3), 'comments') //each post has 3 comments
+            ->state([
+                'title' => 'untitled_02222'
+            ])
+            ->create();
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
