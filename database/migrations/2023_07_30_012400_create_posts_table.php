1a4685a791b3f024b92d8d09ec19787cda7f01ee
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-08-05 14:52:56 +0800

    Update 2023_07_30_012400_create_posts_table.php

--- database/migrations/2023_07_30_012400_create_posts_table.php
+++ database/migrations/2023_07_30_012400_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->json('body')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
